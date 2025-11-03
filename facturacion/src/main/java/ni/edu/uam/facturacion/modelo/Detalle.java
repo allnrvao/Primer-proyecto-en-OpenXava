b21eff81c94ecb4d3cf22e8d42b34e01f8a29fb7
allnrvao <aracuna@uamv.edu.ni>
2025-11-03 16:23:48 -0600

    se refina la interfaz y se agregan nuevas clases, detalle, CalculadorSiguienteNumeroParaAnyo. etc

---
+++ facturacion/src/main/java/ni/edu/uam/facturacion/modelo/Detalle.java
@@ -0,0 +1,20 @@
+package ni.edu.uam.facturacion.modelo;
+
+import javax.persistence.*;
+import lombok.*;
+import org.openxava.annotations.ListProperties;
+
+import java.util.Collection;
+
+@Embeddable @Getter @Setter
+public class Detalle {
+
+    int cantidad;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    Producto producto;
+
+    @ElementCollection
+    @ListProperties("producto.numero, producto.descripcion, cantidad")
+    Collection<Detalle> detalles;
+}
